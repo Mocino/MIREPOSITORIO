9780fc859353f7be03159f1155c913486a91aaf4
unknown <edrson2@gmail.com>
2025-01-21 22:48:27 -0600

    feat: add dashboard y subrutas

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SidemenuComponent } from '@shared/sidemenu/sidemenu.component';
 
 @Component({
-  selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, SidemenuComponent],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styles: ''
 })
 export default class DashboardComponent {
 
