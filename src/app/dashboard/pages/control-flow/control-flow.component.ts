6532a09b5aacab1780ac2e548c476af0ad90c84b
unknown <edrson2@gmail.com>
2025-01-22 11:20:21 -0600

    feat: more control-flow's examples

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 type Grade = 'A'|'B'|'C';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TitleComponent],
   styles: ``,
   templateUrl: './control-flow.component.html',
 })
