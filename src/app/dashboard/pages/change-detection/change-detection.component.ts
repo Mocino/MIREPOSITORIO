fec5850d945ebfa2673e261b59945856edc45f83
Vijardy Garcia <garciavijardy97@deerviewint.com>
2025-01-27 21:43:05 -0600

    feat: change detection

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,12 +1,41 @@
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './change-detection.component.html',
-  styles: ``,
+  imports: [CommonModule, TitleComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `<app-title [title]="currentFramework()"></app-title>
+
+  <pre>{{frameworkAsSignal()    | json}}</pre>
+  <pre>{{frameworkAsPropiedad | json}}</pre>
+  `,
 })
 export default class ChangeDetectionComponent {
 
+  public currentFramework = computed(
+    () => `Change detection - ${this.frameworkAsSignal().name} `
+  );
+
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2026,
+  })
+
+  public frameworkAsPropiedad ={
+    name: 'Angular',
+    releaseDate: 2026,
+  };
+
+  constructor() {
+    setTimeout(() => {
+      // this.frameworkAsPropiedad.name = 'Reat';
+      this.frameworkAsSignal.update( value => ({
+        ...value,
+        name: 'react'
+      }))
+      console.log('hecho');
+    }, 3000)
+  }
 }
